7bef45d05eb31678df74c7227b024bf0d6551dc8
rentao <tao@rtaoo.com>
2026-02-26 19:10:11 +0800

    Add phone to auboi10

--- src/lerobot/teleoperators/phone/phone_processor.py
+++ src/lerobot/teleoperators/phone/phone_processor.py
@@ -84,9 +84,9 @@ class MapPhoneActionToRobotAction(RobotActionProcessorStep):
         action["target_x"] = pos[1] if enabled else 0.0
         action["target_y"] = -pos[0] if enabled else 0.0
         action["target_z"] = pos[2] if enabled else 0.0
-        action["target_wx"] = rotvec[1] if enabled else 0.0
-        action["target_wy"] = rotvec[0] if enabled else 0.0
-        action["target_wz"] = -rotvec[2] if enabled else 0.0
+        action["target_wx"] = -rotvec[1] if enabled else 0.0
+        action["target_wy"] = -rotvec[0] if enabled else 0.0
+        action["target_wz"] = rotvec[2] if enabled else 0.0
         action["gripper_vel"] = gripper_vel  # Still send gripper action when disabled
         return action
 
